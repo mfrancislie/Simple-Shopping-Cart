cc1e57aedcb54f084a95c9592b8c038236dec3fc
Francislie <mfrancislie@gmail.com>
2023-06-10 02:43:09 -0700

    added Main component and render products

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Header from './components/Header';
 import Basket from './components/Basket';
 import Main from './components/Main';
+import data from './data';
 
 const App = () => {
+  const { products } = data;
   return (
     <div>
       <Header />
       <div className="row">
-        <Main />
+        <Main products={products} />
         <Basket />
       </div>
     </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const Main = () => {
-  return <div className="col-2 block">Main</div>;
+const Main = (props) => {
+  const { products } = props;
+  return (
+    <div className="col-2 block">
+      <h1>Products</h1>
+      <div className="row">
+        {products.map((product) => (
+          <h1>{product.name}</h1>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Main;
